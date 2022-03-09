1e59cd6bb73e179520695e1e2a6f2aa21610039e
wahidabd <wahed.blog99@gmail.com>
2022-03-09 20:32:24 +0700

    add soal1

---
+++ soal1.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <wait.h>
+
+void abjad_shift_1() {
+  int i;
+
+  for (i = 65; i < 78; i++) {
+    printf("%c\n", i);
+//	printf("A");
+  }
+}
+
+void abjad_shift_2() {
+  int i;
+  for (i = 78; i < 91; i++) {
+    printf("%c\n", i);
+//	printf("B");
+  }
+}
+
+int main() {
+  int status;
+  pid_t child_id;
+
+  child_id = fork();
+
+  if (child_id < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (child_id == 0) {
+    abjad_shift_1();
+  } else {
+    while ((wait(&status)) > 0);
+    abjad_shift_2();
+  }
+}
